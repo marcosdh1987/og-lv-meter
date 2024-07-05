d9bd91dbe22412057f8860c2cd166254bea1b23f
Marcos <marcos.esteban.soto@gmail.com>
2024-07-05 09:36:02 -0300

    new version of dataplc read

--- src/modbus_adv_toolbox/wenlen_plc.py
+++ src/modbus_adv_toolbox/wenlen_plc.py
@@ -1,42 +1,21 @@
 import logging
 from pymodbus.client.sync import ModbusTcpClient
-from pymodbus.constants import Endian
 from pymodbus.exceptions import ConnectionException, ModbusIOException
-from pymodbus.payload import BinaryPayloadDecoder, BinaryPayloadBuilder
 
 class WenlenPLC:
-    _instance = None
-
-    def __new__(cls, connection_type="TCP", ip_address=None, port=None, retries=2):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._initialize(connection_type, ip_address, port, retries)
-        return cls._instance
-
-    def _initialize(self, connection_type, ip_address, port, retries):
+    def __init__(self, ip_address, port, retries=2):
         self.retries = retries
         self.logger = logging.getLogger("WenlenPLC")
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
-        self.connection_type = connection_type
-        if connection_type == "TCP":
-            self.client = ModbusTcpClient(ip_address, port=port)
-        else:
-            self.logger.error(f"Unsupported connection type: {connection_type}")
-            raise ValueError("Unsupported connection type")
+        self.client = ModbusTcpClient(ip_address, port=port)
 
     def update_ip_and_port(self, new_ip_address, new_port):
-        if self.connection_type != "TCP":
-            self.logger.error("Cannot update IP and Port for non-TCP connection")
-            return
-        else:
-            self.client = ModbusTcpClient(
+        self.client = ModbusTcpClient(
                 host=new_ip_address, port=new_port, auto_open=True, auto_close=True
             )
 
@@ -56,9 +35,10 @@ class WenlenPLC:
         self.client.close()
         self.logger.info("Connection closed")
 
-    def read_coils(self, start_reg, count=32, unit=1):
+    def read_coils(self, start_reg, count=1, unit=1):
         try:
-            result = self.client.read_coils(start_reg - 1, count, unit=unit)
+            self.logger.debug(f"Attempting to read {count} coils starting at {start_reg} with unit {unit}")
+            result = self.client.read_coils(start_reg, count, unit=unit)
             if not result.isError():
                 return result.bits
             else:
@@ -71,9 +51,10 @@ class WenlenPLC:
             self.logger.error(f"Failed to read coils: {e}")
             return None
 
-    def read_holding_registers(self, start_reg, length=2, unit=1):
+    def read_holding_registers(self, start_reg, length=1, unit=1):
         try:
-            regs = self.client.read_holding_registers(start_reg - 1, length, unit=unit)
+            self.logger.debug(f"Attempting to read {length} holding registers starting at {start_reg} with unit {unit}")
+            regs = self.client.read_holding_registers(start_reg, length, unit=unit)
             if not regs.isError():
                 return regs.registers
             else:
@@ -86,49 +67,83 @@ class WenlenPLC:
             self.logger.error(f"Failed to read holding registers: {e}")
             return None
 
-    def decode_register_cdab(self, registers):
-        decoder = BinaryPayloadDecoder.fromRegisters(
-            registers, byteorder=Endian.Big, wordorder=Endian.Little
-        )
-        return decoder.decode_32bit_float()
+    def read_input_registers(self, start_reg, length=1, unit=1):
+        try:
+            self.logger.debug(f"Attempting to read {length} input registers starting at {start_reg} with unit {unit}")
+            regs = self.client.read_input_registers(start_reg, length, unit=unit)
+            if not regs.isError():
+                return regs.registers
+            else:
+                self.logger.error(f"Failed to read input registers: {regs}")
+                return None
+        except ModbusIOException as e:
+            self.logger.error(f"Modbus IO Error: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"Failed to read input registers: {e}")
+            return None
 
-    def read_register_with_retries(self, start_reg, length=2, unit=1):
-        for attempt in range(self.retries):
-            result = self.read_holding_registers(start_reg, length, unit)
-            if result is not None:
-                return result
-            self.logger.warning(
-                f"Attempt {attempt + 1} failed for register {start_reg}. Retrying..."
-            )
-        self.logger.error(
-            f"Failed to read register {start_reg} after {self.retries} attempts."
-        )
-        return None
+    def read_discrete_inputs(self, start_reg, count=1, unit=1):
+        try:
+            self.logger.debug(f"Attempting to read {count} discrete inputs starting at {start_reg} with unit {unit}")
+            result = self.client.read_discrete_inputs(start_reg, count, unit=unit)
+            if not result.isError():
+                return result.bits
+            else:
+                self.logger.error(f"Failed to read discrete inputs: {result}")
+                return None
+        except ModbusIOException as e:
+            self.logger.error(f"Modbus IO Error: {e}")
+            return None
+        except Exception as e:
+            self.logger.error(f"Failed to read discrete inputs: {e}")
+            return None
 
-    def read_boolean_registers(self, start_reg=10001, count=32, unit=1):
-        return self.read_coils(start_reg, count, unit)
-
-    def read_float_registers(self, start_reg=40001, count=4, unit=1):
-        floats = []
+    def read_boolean_registers(self, start_reg=10001, count=32):
+        first_values = []
         for i in range(count):
-            regs = self.read_register_with_retries(start_reg + 2 * i, length=2, unit=unit)
-            self.logger.info(f"Read registers for float {i}: {regs}")
-            if regs:
-                value = self.decode_register_cdab(regs)
-                floats.append(value)
-                self.logger.info(f"Decoded float value {i}: {value}")
+            discrete_input = self.read_discrete_inputs(start_reg=i, count=8)
+            if discrete_input is not None:
+                first_values.append(discrete_input[0])
+            else:
+                first_values.append(None)
+        return first_values
+    
+    def combine_registers_to_floats(self, registers):
+        floats = []
+        for i in range(0, len(registers), 2):
+            if registers[i] is not None and registers[i+1] is not None:
+                combined_value = float(f"{registers[i]}.{registers[i+1]:02d}")
+                floats.append(combined_value)
             else:
                 floats.append(None)
-                self.logger.warning(f"Failed to read or decode float value {i}")
         return floats
 
+    def read_holding_registers_values(self):
+        holding_values = []
+        for i in range(8):
+            holding_register = self.read_holding_registers(start_reg=i, length=1)
+            if holding_register is not None:
+                holding_values.append(holding_register[0])
+            else:
+                holding_values.append(None)
+        return holding_values
+    
+    def read_float_registers(self, start_reg=40001, count=4):
+        holding_registers = self.read_holding_registers_values()
+        float_registers = self.combine_registers_to_floats(holding_registers)
+        return float_registers
+    
     def write_coils(self, start_reg, values, unit=1):
         try:
-            result = self.client.write_coils(start_reg - 1, values, unit=unit)
-            if result.isError():
+            self.logger.debug(f"Attempting to write coils starting at {start_reg} with unit {unit}")
+            result = self.client.write_coils(start_reg, values, unit=unit)
+            if not result.isError():
+                self.logger.info("Write coils successful")
+                return True
+            else:
                 self.logger.error(f"Failed to write coils: {result}")
                 return False
-            return True
         except ModbusIOException as e:
             self.logger.error(f"Modbus IO Error: {e}")
             return False
@@ -138,19 +153,34 @@ class WenlenPLC:
 
     def write_register(self, start_reg, value, unit=1):
         try:
-            builder = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
-            builder.add_32bit_float(value)
-            payload = builder.to_registers()
-            self.logger.info(f"Writing float value {value} to registers starting at {start_reg}: {payload}")
-            result = self.client.write_registers(start_reg - 1, payload, unit=unit)
-            self.logger.info(f"Write result for registers starting at {start_reg}: {result}")
-            if result.isError():
+            self.logger.debug(f"Attempting to write register at {start_reg} with unit {unit}")
+            result = self.client.write_register(start_reg, value, unit=unit)
+            if not result.isError():
+                self.logger.info("Write register successful")
+                return True
+            else:
                 self.logger.error(f"Failed to write register: {result}")
                 return False
-            return True
         except ModbusIOException as e:
             self.logger.error(f"Modbus IO Error: {e}")
             return False
         except Exception as e:
             self.logger.error(f"Failed to write register: {e}")
+            return False
+        
+    def write_discrete_inputs(self, start_reg, values, unit=1):
+        try:
+            self.logger.debug(f"Attempting to write discrete inputs starting at {start_reg} with unit {unit}")
+            result = self.client.write_coils(start_reg, values, unit=unit)  # Reuse write_coils for discrete inputs
+            if not result.isError():
+                self.logger.info(f"Successfully wrote discrete inputs: {values}")
+                return True
+            else:
+                self.logger.error(f"Failed to write discrete inputs: {result}")
+                return False
+        except ModbusIOException as e:
+            self.logger.error(f"Modbus IO Error: {e}")
+            return False
+        except Exception as e:
+            self.logger.error(f"Failed to write discrete inputs: {e}")
             return False
